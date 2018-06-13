ecf2ff0ee3aa766ee893b06b1f8f0306cd20dc40
Herbert Vasquez <itsherbvasquez@gmail.com>
2018-06-13 13:56:44 -0400

    significant changes to the project in all files

--- databass.js
+++ databass.js
@@ -7,3 +7,17 @@ function initMap() {
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
+
+
+var location=[{
+    longitude: "37.510958",
+      latitude: "-77.459601",
+        date1: "06/16/2018",
+          date2: "06/14/2018",
+            code1: "8037",
+              code2:"1223"
+            }]
+$("#SUBMIT").click(function){for (i=0; i<location.length; i++){
+  if(code1 = date1)
+  }
+}
